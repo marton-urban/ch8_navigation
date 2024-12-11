0f98b91232e66345a85bcfc0c2e5368023a3e49a
Marton Urban <urban.marton@gmail.com>
2024-12-11 10:44:04 +0100

    fix naming and path

--- test/repositories/auth_repository_test.dart
+++ test/repositories/auth_repository_test.dart
@@ -2,7 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:yummy/repositories/auth_repository.dart';
 
 import 'package:mocktail/mocktail.dart';
-import '../../../mocks.dart';
+
+import '../mocks.dart';
 
 void main() {
   late AuthRepository authRepository;
